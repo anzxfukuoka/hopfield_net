d6101ad5d17f1705cc5682647e85865d008df2f0
ANZX <anzx0@outlook.com>
2022-12-12 15:56:44 +0100

    feat: learn and recognize function added

--- hopfield.py
+++ hopfield.py
@@ -1,20 +1,69 @@
+import os.path
+
 import numpy as np
 from PIL import Image
+import glob
 
 
-def image_to_data(img):
+
+def image_to_data(img: Image):
+    """
+    Converts PIL Image to np.Array
+    :return: converted image array
+    """
     data = np.array(img.convert("L"))
     sign = np.vectorize(lambda x: 1 if x < 128 else -1)
     data = sign(data)
+    data = data.flatten()
     return data
 
 
-class HopNet:
-    def __init__(self):
-        pass
+def load_images_data(folder_path: str):
+    f"""
+    Loads images data from folder
+    :param folder_path: path to data folder
+    :return: dict -> filename: data 
+    """
+    images_data = {}
+
+    for filename in glob.glob(os.path.join(folder_path, "*.png")):
+        im = Image.open(filename)
+        data = image_to_data(im)
+        images_data.update({filename: data})
+
+    return images_data
+
+
+class HopfieldNetwork:
+    """
+    Implementation of Hopfield network
+    """
+    def __init__(self, images_to_learn: dict, image_size: int = 5):
+        self.images_to_learn = images_to_learn
+
+        self.images_data = np.array(list(images_to_learn.values()))
+
+        self.image_size = image_size
+        self.images_count = len(images_to_learn)
+
+        # weights of learned images
+        self.W = np.zeros((self.image_size * self.image_size, self.image_size * self.image_size))
 
     def learn(self):
-        pass
 
-    def recognise(self):
+        for i in range(1, self.images_count):
+            a = np.array([self.images_data[i - 1]])
+            b = np.array([self.images_data[i]])
+            mul = np.matmul(a, b.T)
+            self.W += mul
+
+        np.fill_diagonal(self.W, 0)
+        self.W /= self.image_size
+
+    def recognize(self, image_data):
+        recognized = np.matmul(self.W, np.array(image_data).T)
+        sign = np.vectorize(lambda x: 1 if x < 0 else -1)
+        recognized = sign(recognized)
+        recognized = recognized.reshape((5, 5))
+        print(recognized)
         pass
--- server.py
+++ server.py
@@ -7,10 +7,16 @@ import base64
 import json
 import re
 
-from hopfield import image_to_data
+from hopfield import HopfieldNetwork, image_to_data, load_images_data
 
+# creation of hopfield net
+images_data_folder = "images_data/"
+images_data = load_images_data(images_data_folder)
+hopNet = HopfieldNetwork(images_data)
+hopNet.learn()
+
+# flask server for web interface
 app = Flask("Hopfield Network")
-
 
 @app.route("/")
 def index():
@@ -22,7 +28,8 @@ def save_canvas():
     image_data = re.sub('^data:image/.+;base64,', '', request.form['imageBase64'])
     im = Image.open(BytesIO(base64.b64decode(image_data)))
     #im.save('canvas.png')
-    image_to_data(im)
+    data = image_to_data(im)
+    hopNet.recognize(data)
     return json.dumps({'result': 'success'}), 200, {'ContentType': 'application/json'}
 
 
